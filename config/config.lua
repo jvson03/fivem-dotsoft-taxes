86b9ff82a1f1c446cf35898b4371e66a4a379bb1
Jvson03 <jvsonlamaj@gmail.com>
2023-08-29 18:37:54 +0100

    feat(tax): different amount per category

--- config/config.lua
+++ config/config.lua
@@ -12,6 +12,15 @@ Config.OwnedVehiclesTable = 'player_vehicles'
 -- Tax per vehicle owned
 Config.VehicleTax = math.random (20, 40)
 
+-- Taxes for each vehicle type (!WIP)
+Config.Tax = {
+    Automobile = math.random (20, 40),
+    Bike = math.random (15, 35),
+    Boat = math.random (450, 800),
+    Heli = math.random (600, 1200),
+    Plane = math.random (700, 1800)
+}
+
 -- [House Tax] --
 Config.EnableHouseTax = true
 
